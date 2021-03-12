98dc7f249251f75acfeb86da6bce38eef9c0974b
root <root@dp.ubiz.fr>
2021-03-12 23:15:03 +0100

    Résidu_Quadratique

---
+++ TP_residu_quadratique/tp_residu_quadratique.h
@@ -0,0 +1,9 @@
+#ifndef H_TP_RESIDU_QUADRATIQUE
+#define H_TP_RESIDU_QUADRATIQUE
+
+int phi(int n);
+int fact_premier(int n);
+int residu_quadratique(int n,int **cs);
+int Legendre(long unsigned int a,long unsigned int p);
+
+#endif
